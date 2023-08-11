b062b3bfb35ee47268d89ea2de9c96386a6b58c5
solomonbaez <solomon.james.baez@gmail.com>
2023-08-11 13:08:53 -0700

    Restructured /posts with a route layout

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,13 +1,72 @@
 import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+const name: string = "Admin";
+export const siteTitle: string = "NextBlog";
 
 export default function PostLayout({
   children,
+  home,
 }: {
   children: React.ReactNode;
+  home: React.ReactNode;
 }) {
   return (
-    <div className=" flex items-center justify-center h-screen ">
-      {children}
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <Head className="flex-col items-center">
+          <title>First Post</title>
+        </Head>
+        <header className="flex items-center">
+          {home ? (
+            <>
+              <div>
+                <Image
+                  priority
+                  src="/images/Aelionore.jpg"
+                  className="rounded-full"
+                  height={144}
+                  width={144}
+                  alt=""
+                />
+                <h1 className="text-2xl text-w leading-4 tracking-tighter m-1rem-0">
+                  {name}
+                </h1>
+              </div>
+            </>
+          ) : (
+            <>
+              <div>
+                <Link href="/">
+                  <Image
+                    priority
+                    src="/images/Aelionore.jpg"
+                    className="rounded-full"
+                    height={108}
+                    width={108}
+                    alt=""
+                  />
+                </Link>
+                <h2 className="text-base text-w leading-4 tracking-tighter m-1rem-0">
+                  <Link href="/" className="text-inherit">
+                    {name}
+                  </Link>
+                </h2>
+              </div>
+            </>
+          )}
+        </header>
+        <main>{children}</main>
+        {!home && (
+          <div className="m-3rem">
+            <Link className="text-sky-400 hover:underline font-bold" href="/">
+              Back to Home
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -6,25 +6,8 @@ import PostLayout from "./layout";
 // function declarations don't need closing colons
 export default function FirstPost() {
   return (
-    <PostLayout>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <div>
-        <Image
-          className=" h-300 w-300 display-block "
-          src="/images/Aelionore.jpg"
-          width={100}
-          height={100}
-          alt="PFP"
-        />
-        <h1>First Post</h1>
-        <h2>
-          <Link className=" font-bold underline " href="/">
-            Back to Home
-          </Link>
-        </h2>
-      </div>
-    </PostLayout>
+    <main>
+      <h1>First Post</h1>
+    </main>
   );
 }
